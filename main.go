58afb6f77c8ba9944d9bf37f44270a86ed01d99d
Dassyareg <dassyareg@gmail.com>
2023-01-04 13:52:59 +0100

    Brute force

--- main.go
+++ main.go
@@ -1,19 +1,17 @@
 package main
 
 import (
-	"html/template"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"time"
 )
 
-var tpl *template.Template
 var hostname string
 var err error
 
 func init() {
-	tpl = template.Must(template.ParseFiles("index.gohtml"))
 	hostname, err = os.Hostname(); if err != nil {
 		log.Fatalln("Unable to retrieve hostname")
 	}
@@ -36,7 +34,20 @@ func main() {
 func indexHandler(w http.ResponseWriter, req *http.Request) {
 	c, err := req.Cookie("hsn")
 	if err == nil {
-		tpl.Execute(w, c.Value)
+		io.WriteString(w, `<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Get Hostname</title>
+		</head>
+		<body>
+			
+			<h3>My Hostname is ` +  c.Value + ` </h3>
+		
+		</body>
+		</html>`)
 		return
 	}
 
@@ -48,5 +59,19 @@ func indexHandler(w http.ResponseWriter, req *http.Request) {
 
 	http.SetCookie(w, c)
 
-	tpl.Execute(w, c.Value)
+	io.WriteString(w, `<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Get Hostname</title>
+		</head>
+		<body>
+			
+			<h3>My Hostname is ` +  c.Value + ` </h3>
+		
+		</body>
+		</html>`)
+
 }
